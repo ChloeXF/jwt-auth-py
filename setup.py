2b130d97b8de42649dc703db15f9e6fb9730a37b
chenhaiou <chenhaiou@qusong.com>
2022-11-16 14:52:29 +0800

    setup.py: rename jwt_auths

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup
 
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
-    name="jwt_auth",
+    name="jwt_auths",
+    version='0.0.1',
     install_requires=[
         "Django>=2.0.5",
         "djangorestframework>=3.8.2",
