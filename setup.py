11e8dc79e8ecfd54419941502b8dc06c8ebcda3c
chenhaiou <chenhaiou@qusong.com>
2022-11-16 15:14:30 +0800

    setup.py: update version to 0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="jwt_auths",
-    version='0.0.1',
+    version='0.0.3',
+    url='https://github.com/ChloeXF/jwt-auth-py',
+    packages=['jwt_auth'],
     install_requires=[
         "Django>=2.0.5",
         "djangorestframework>=3.8.2",
